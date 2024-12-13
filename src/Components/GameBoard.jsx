cfb5fe6632e25167af049371fed7bda8071de0aa
Pragya-10 <pragyamishra10501@gmail.com>
2024-12-13 23:58:43 +0530

    improving functionality

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -9,7 +9,12 @@ const initialGameBoard = [
 export default function GameBoard({onSelectSquare, turns}) {
 
   let gameBoard = initialGameBoard;
-  for(const turn of turns)
+  for(const turn of turns) {
+    const {square, player} = turn;
+    const {row, col} = square;
+
+    gameBoard[row][col] = player;
+  }
 
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
@@ -30,7 +35,7 @@ export default function GameBoard({onSelectSquare, turns}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={()=>handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
